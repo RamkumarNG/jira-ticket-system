fa4e1c6449e3ec2d575c75ec224244fda8039daa
ramkumar@madstreetden.com <ramkumar@madstreetden.com>
2025-07-20 11:56:03 +0530

    added dockerfile and scrip file for migrations

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -8,10 +8,12 @@ from alembic import context
 
 from core.database.models import mapper_registry
 
+from core.config import settings
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
+config.set_main_option("sqlalchemy.url", settings.SYNC_DB_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
