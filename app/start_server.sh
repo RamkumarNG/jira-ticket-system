592b454f6a7bef7f9ead1cf6edff2c25c5a7e6db
ramkumar@madstreetden.com <ramkumar@madstreetden.com>
2025-07-20 11:45:32 +0530

    added dockerfile and scrip file for migrations

--- app/start_server.sh
+++ app/start_server.sh
@@ -4,11 +4,10 @@ set -e
 
 echo "=== Current working directory: $(pwd) ==="
 echo "=== Files in current directory ==="
-ls -l
+ls -l app/alembic.ini
 
 echo "=== Running Alembic migrations ==="
-# alembic -c app/alembic.ini upgrade head
-(cd /app/ && alembic upgrade head)
+/root/.local/bin/alembic -c /app/app/alembic.ini upgrade head
 
 echo "=== Starting FastAPI application ==="
 uvicorn app.main:app --host 0.0.0.0 --port 10000
