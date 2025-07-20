624173c5c847a52f0ef98894689729acf2acad76
ramkumar@madstreetden.com <ramkumar@madstreetden.com>
2025-07-20 12:54:17 +0530

    start_server fix

--- app/start_server.sh
+++ app/start_server.sh
@@ -12,4 +12,4 @@ python -m alembic -c alembic.ini upgrade head
 
 
 echo "=== Starting FastAPI application ==="
-uvicorn app.main:app --host 0.0.0.0 --port 10000
+uvicorn main:app --host 0.0.0.0 --port 10000
