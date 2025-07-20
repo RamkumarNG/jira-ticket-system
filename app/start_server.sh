4f3dc1691890ebf35b1fc3a64bcb6ee21c901ef7
ramkumar@madstreetden.com <ramkumar@madstreetden.com>
2025-07-20 11:33:39 +0530

    added dockerfile and scrip file for migrations

--- app/start_server.sh
+++ app/start_server.sh
@@ -3,7 +3,7 @@
 set -e  # Exit on any error
 
 echo "=== Running Alembic migrations ==="
-alembic upgrade head
+alembic -c app/alembic.ini upgrade head
 
 echo "=== Starting FastAPI application ==="
 uvicorn app.main:app --host 0.0.0.0 --port 10000
